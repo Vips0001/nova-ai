8f24b02ba97c9604c7c26e43c9d5c052b3aa2d93
Vips0001 <pensdriving@gmail.com>
2026-08-09 00:59:18 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,49 @@
 const CACHE_NAME = "nova-ai-v1";
 
-const FILES = [
+const FILES_TO_CACHE = [
     "./",
     "./index.html",
     "./style.css",
     "./script.js",
-    "./manifest.json"
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
 ];
 
+// Install service worker
 self.addEventListener("install", event => {
     event.waitUntil(
         caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(FILES))
+            .then(cache => {
+                return cache.addAll(FILES_TO_CACHE);
+            })
     );
+
+    self.skipWaiting();
 });
 
+// Activate service worker
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames
+                    .filter(name => name !== CACHE_NAME)
+                    .map(name => caches.delete(name))
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+// Serve cached files when possible
 self.addEventListener("fetch", event => {
     event.respondWith(
         caches.match(event.request)
-        .then(response => response || fetch(event.request))
+            .then(response => {
+                return response || fetch(event.request);
+            })
     );
 });
+   
